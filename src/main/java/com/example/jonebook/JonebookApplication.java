ce8dfbca105b13b454379c3af640551413bfe1a2
Chamber6821 <vania.osinin@yandex.ru>
2022-11-06 15:09:45 +0300

    Init commit

---
+++ src/main/java/com/example/jonebook/JonebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.jonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JonebookApplication.class, args);
+    }
+
+}
