cd7780778c210c5feb9ef20f4d033aad255b3723
Chamber6821 <vania.osinin@yandex.ru>
2022-11-09 01:08:47 +0300

    chore: hello world with Security

---
+++ src/main/java/com/example/jonebook/config/SpringSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.jonebook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/welcome")
+                .hasRole("ADMIN")
+                .antMatchers("/")
+                .hasRole("USER")
+                .and()
+                .formLogin();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+
+    }
+}
--- src/main/java/com/example/jonebook/web/WelcomeController.java
+++ src/main/java/com/example/jonebook/web/WelcomeController.java
@@ -1,7 +1,5 @@
 package com.example.jonebook.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WelcomeController {
 
-    Logger logger = LoggerFactory.getLogger(LoggerFactory.class);
-
-    @GetMapping("/welcome")
+    @GetMapping({"/", "/welcome"})
     public String welcome(@RequestParam(defaultValue = "stranger") String name, Model model) {
-        logger.info("Requested: /welcome");
+
         model.addAttribute("name", name);
         return "welcome.jsp";
     }
