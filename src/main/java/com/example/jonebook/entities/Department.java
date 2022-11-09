beb7f8095cea8bf38a6f8d6525aa70016245bcf4
Chamber6821 <vania.osinin@yandex.ru>
2022-11-09 22:44:44 +0300

    fix: set id auto increment for all entities

--- src/main/java/com/example/jonebook/entities/Department.java
+++ src/main/java/com/example/jonebook/entities/Department.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Entity
 public class Department {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
--- src/main/java/com/example/jonebook/entities/Employee.java
+++ src/main/java/com/example/jonebook/entities/Employee.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Entity
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
