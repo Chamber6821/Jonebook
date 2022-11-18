c45c886c318e0b08324e0200301870ccfb83b8d0
Chamber6821 <vania.osinin@yandex.ru>
2022-11-18 12:27:15 +0300

    fix: add sorting to PublicEmployeeController

--- src/main/java/com/example/jonebook/api/v1/PublicEmployeeController.java
+++ src/main/java/com/example/jonebook/api/v1/PublicEmployeeController.java
@@ -1,8 +1,11 @@
 package com.example.jonebook.api.v1;
 
+import com.example.jonebook.entities.Employee;
 import com.example.jonebook.repositories.EmployeeRepository;
 import com.example.jonebook.services.dto.PublicEmployee;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +28,7 @@ public class PublicEmployeeController {
     @GetMapping
     public List<PublicEmployee> getAll(@RequestParam(defaultValue = "100") int pageSize,
                                        @RequestParam(defaultValue = "1") int page) {
-        Pageable pageable = Pageable.ofSize(Math.min(pageSize, 100)).withPage(page);
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.Direction.ASC, Employee.Fields.id);
         return employees.findAll(pageable).stream()
                 .map(PublicEmployee::new)
                 .toList();
