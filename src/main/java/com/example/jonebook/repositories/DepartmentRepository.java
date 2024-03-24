8d8e9101376351c0a0fb31b58c13394d63191938
Chamber6821 <vania.osinin@yandex.ru>
2024-03-24 16:10:12 +0300

    feat(DepartmentRepository): custom SQL query

--- src/main/java/com/example/jonebook/repositories/DepartmentRepository.java
+++ src/main/java/com/example/jonebook/repositories/DepartmentRepository.java
@@ -3,6 +3,8 @@ package com.example.jonebook.repositories;
 import com.example.jonebook.entities.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +14,6 @@ import java.util.Optional;
 public interface DepartmentRepository extends JpaRepository<Department, Long>, JpaSpecificationExecutor<Department> {
     Optional<Department> findByName(String name);
 
-    List<Department> findByNameStartingWithOrderByName(String namePrefix);
+    @Query("SELECT d FROM Department d WHERE name LIKE '%:prefix' ORDER BY name")
+    List<Department> findByNameStartingWithOrderByName(@Param("prefix") String namePrefix);
 }
