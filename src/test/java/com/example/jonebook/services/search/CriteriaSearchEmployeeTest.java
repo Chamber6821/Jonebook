0b6bebe41dbb63b54c4e7a24e55873811a812710
Chamber6821 <vania.osinin@yandex.ru>
2024-04-18 02:46:03 +0300

    test(CriteriaSearchEmployee): search something and call criteria builder

--- src/test/java/com/example/jonebook/services/search/CriteriaSearchEmployeeTest.java
+++ src/test/java/com/example/jonebook/services/search/CriteriaSearchEmployeeTest.java
@@ -2,17 +2,35 @@ package com.example.jonebook.services.search;
 
 import static org.mockito.Mockito.*;
 
+import com.example.jonebook.entities.Employee;
 import com.example.jonebook.repositories.EmployeeRepository;
 import com.example.jonebook.services.dto.EmployeeCriteria;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 public class CriteriaSearchEmployeeTest {
+  abstract class PartialEmployeeRepository implements EmployeeRepository {
+    @Override
+    public Page<Employee> findAll(Specification<Employee> spec,
+        Pageable pageable) {
+      spec.toPredicate(mock(Root.class), mock(CriteriaQuery.class),
+          mock(CriteriaBuilder.class));
+      return Page.<Employee>empty();
+    }
+  }
+
   @Test
   void searchAll() {
-    new CriteriaSearchEmployee(mock(EmployeeRepository.class))
-        .search(EmployeeCriteria.builder()
+    var employees = mock(PartialEmployeeRepository.class);
+    new CriteriaSearchEmployee(employees).search(
+        EmployeeCriteria.builder()
             .nameFragment("Petr")
             .emailFragment("@")
             .phonePrefix("123")
@@ -28,6 +46,6 @@ public class CriteriaSearchEmployeeTest {
               }
             })
             .build(),
-            null);
+        null);
   }
 }
