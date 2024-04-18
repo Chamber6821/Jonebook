95236747bf9daca36d24e152578d07cd5a398bc4
Chamber6821 <vania.osinin@yandex.ru>
2024-04-18 13:32:43 +0300

    refactor(EmployeeRepository): replace String concatenation with multiline string

--- src/main/java/com/example/jonebook/repositories/EmployeeRepository.java
+++ src/main/java/com/example/jonebook/repositories/EmployeeRepository.java
@@ -1,9 +1,7 @@
 package com.example.jonebook.repositories;
 
 import com.example.jonebook.entities.Employee;
-
 import java.util.Set;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,27 +10,29 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
-    @Query("SELECT e FROM Employee e\n"
-            + "LEFT JOIN e.department d\n"
-            + "WHERE lower(e.name) LIKE lower(concat('%', :nameFragment, '%'))\n"
-            + "AND lower(e.email) LIKE lower(concat('%', :emailFragment, '%'))\n"
-            + "AND e.phone LIKE lower(concat(:phonePrefix, '%'))\n"
-            + "AND e.internalPhone LIKE lower(concat('%', :internalPhonePrefix, '%'))\n"
-            + "AND (:departmentVariants IS NULL OR d.name IN (:departmentVariants))\n"
-            + "AND NOT EXISTS (\n"
-            + "     SELECT p.id FROM WorkPost p\n"
-            + "     WHERE p.name IN (:postsFragment)\n"
-            + "     AND p.id NOT IN (\n"
-            + "         SELECT p2.id FROM Employee e2\n"
-            + "         JOIN e2.posts p2 WHERE e2 = e))\n"
-            + "GROUP BY e.id\n")
-    public Page<Employee> findAllByMyCoolestInternationalQueryByAllDataInTheWord(
-            String nameFragment,
-            String emailFragment,
-            String phonePrefix,
-            String internalPhonePrefix,
-            Set<String> departmentVariants,
-            Set<String> postsFragment,
-            Pageable pageable);
+public interface EmployeeRepository
+    extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
+        @Query("""
+                        SELECT e FROM Employee e
+                            LEFT JOIN e.department d
+                            WHERE lower(e.name) LIKE lower(concat('%', :nameFragment, '%'))
+                            AND lower(e.email) LIKE lower(concat('%', :emailFragment, '%'))
+                            AND e.phone LIKE lower(concat(:phonePrefix, '%'))
+                            AND e.internalPhone LIKE lower(concat('%', :internalPhonePrefix, '%'))
+                            AND (:departmentVariants IS NULL OR d.name IN (:departmentVariants))
+                            AND NOT EXISTS (
+                                 SELECT p.id FROM WorkPost p
+                                 WHERE p.name IN (:postsFragment)
+                                 AND p.id NOT IN (
+                                     SELECT p2.id FROM Employee e2
+                                     JOIN e2.posts p2 WHERE e2 = e))
+                            GROUP BY e.id""")
+        public Page<Employee> findAllByMyCoolestInternationalQueryByAllDataInTheWord(
+                        String nameFragment,
+                        String emailFragment,
+                        String phonePrefix,
+                        String internalPhonePrefix,
+                        Set<String> departmentVariants,
+                        Set<String> postsFragment,
+                        Pageable pageable);
 }
