1110777385c8ce0be196b91ab78144ba09a47396
Chamber6821 <vania.osinin@yandex.ru>
2024-04-18 03:52:57 +0300

    test(CustomQuerySearchEmployeeTest): select someone

---
+++ src/test/java/com/example/jonebook/services/search/CustomQuerySearchEmployeeTest.java
@@ -0,0 +1,32 @@
+package com.example.jonebook.services.search;
+
+import static org.mockito.Mockito.mock;
+
+import com.example.jonebook.repositories.EmployeeRepository;
+import com.example.jonebook.services.dto.EmployeeCriteria;
+import java.util.HashSet;
+import org.junit.jupiter.api.Test;
+
+public class CustomQuerySearchEmployeeTest {
+  @Test
+  void selectAll() {
+    new CustomQuerySearchEmployee(mock(EmployeeRepository.class))
+        .search(EmployeeCriteria.builder()
+            .nameFragment("Petr")
+            .emailFragment("@")
+            .phonePrefix("123")
+            .internalPhonePrefix("321")
+            .departmentVariants(new HashSet<>() {
+              {
+                add("School");
+              }
+            })
+            .postsFragment(new HashSet<>() {
+              {
+                add("Teacher");
+              }
+            })
+            .build(),
+            null);
+  }
+}
